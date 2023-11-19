c8c9a0e9484d2b2761bda8a4f2fd5192fe42ab58
bisbat <110973307+bisbat@users.noreply.github.com>
2023-11-19 16:35:06 +0700

    Update lab1.ts

--- lab1.ts
+++ lab1.ts
@@ -1,13 +1,5 @@
 function categorizeTemperature(temperature: number): string {
-    if (temperature < 0) {
-      return "Freezing";
-    } else if (temperature >= 0 && temperature < 10) {
-      return "Cold";
-    } else if (temperature >= 10 && temperature < 25) {
-      return "Moderate";
-    } else {
-      return "Hot";
-    }
+    //coding here
   }
   
   // Example usage
